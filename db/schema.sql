857eb1eadb9fd6360b35463fefd781686fdc7c88
The Prattler <jmichaelsmith@juno.com>
2021-12-27 23:52:06 -0500

    added 2 foreign keys, functioning properly

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,6 @@
-DROP TABLE IF EXISTS department;
-DROP TABLE IF EXISTS role;
 DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS department;
 
 CREATE TABLE department (
   id INTEGER PRIMARY KEY AUTO_INCREMENT,
@@ -11,7 +11,8 @@ CREATE TABLE role (
   id INTEGER PRIMARY KEY AUTO_INCREMENT,
   title VARCHAR(30),
   salary DECIMAL,
-  department_id INTEGER
+  department_id INTEGER,
+  FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
@@ -19,5 +20,5 @@ CREATE TABLE employee (
   first_name VARCHAR(30),
   last_name VARCHAR(30),
   role_id INTEGER,
-  manager_id INTEGER
+  FOREIGN KEY (role_id) REFERENCES role(id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -21,18 +21,19 @@ VALUES
   ('Supplier Relations Rep', 35000, 7),
   ('Temporary Worker', 25000, 8);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
+INSERT INTO employee (first_name, last_name, role_id)
 VALUES
-  ('Michael', 'Scott', 1, NULL),
-  ('Dwight', 'Schrute', 2, 1),
-  ('Jim', 'Halpert', 2, 1),
-  ('Stanley', 'Hudson', 2, 1),
-  ('Phyllis', 'Smith', 2, 1),
-  ('Angela', 'Martin', 3, 1),
-  ('Oscar', 'Nunez', 3, 1),
-  ('Kevin', 'Malone', 3, 1),
-  ('Pam', 'Beesly', 6, 1),
-  ('Toby', 'Flenderson', 5, 1),
-  ('Creed', 'Bratton', 7, 1),
-  ('Meredith', 'Palmer', 8, 1),
-  ('Ryan', 'Howard', 9, 1);
+  ('Michael', 'Scott', 1),
+  ('Dwight', 'Schrute', 2),
+  ('Jim', 'Halpert', 2),
+  ('Stanley', 'Hudson', 2),
+  ('Phyllis', 'Smith', 2),
+  ('Angela', 'Martin', 3),
+  ('Oscar', 'Nunez', 3),
+  ('Kevin', 'Malone', 3),
+  ('Pam', 'Beesly', 6),
+  ('Toby', 'Flenderson', 5),
+  ('Creed', 'Bratton', 7),
+  ('Meredith', 'Palmer', 8),
+  ('Ryan', 'Howard', 9),
+  ('Kelly', 'Kapoor', 4);
